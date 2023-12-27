8f19b53917c55b709abb92c40031300bab414a76
Zare2001 <154330044+Zare2001@users.noreply.github.com>
2023-12-27 18:11:18 +0100

    Update EstimatorProject.m
    Added estimation for the PSDs
    Added weiner filter weights for the k-th frequency bin, Sxx(k) is the PSD of the signal, and Nxx(k) is the PSD of the noise at that frequency bin.
    Then the weights are then applied to the DFT of the noisy signal

--- EstimatorProject.m
+++ EstimatorProject.m
@@ -39,23 +39,59 @@ for m = 1:nrmics
         fftResult(:, i, m) = fft(windowedFrame);
     end
 end
-    
-w = complex(rand(nrmics, 1), rand(nrmics, 1)); % Complex noise reduction weights
-    
-for i = 1:numFrames
-    % Initialize a temporary variable to sum the weighted FFT results for this frame
-    combinedFrame = zeros(frameSize, 1);
-    for m = 1:nrmics
-        % Multiply FFT result by the weight for this microphone
-        weightedFrame = fftResult(:, i, m) * w(m);
+   
+%%
 
-        % Sum the weighted results across microphones
-        combinedFrame = combinedFrame + weightedFrame;
+% Estimate PSDs for signal and noise
+Sxx = zeros(K, L);
+Nxx = zeros(K, L);
+for m = 1:nrmics
+    for i = 1:numFrames
+        frameStart = (i-1) * stepSize + 1;
+        frameEnd = frameStart + frameSize - 1;
+
+        % Clean and noisy frames
+        cleanFrame = Clean(frameStart:frameEnd) .* window;
+        noisyFrame = Data(frameStart:frameEnd, m) .* window;
+
+        % FFT
+        cleanFFT = fft(cleanFrame);
+        noisyFFT = fft(noisyFrame);
+
+        % PSD estimation
+        Sxx(:, i) = Sxx(:, i) + abs(cleanFFT).^2 / nrmics;
+        noiseFFT = noisyFFT - cleanFFT;
+        Nxx(:, i) = Nxx(:, i) + abs(noiseFFT).^2 / nrmics;
     end
-    % Store the combined result for this frame
-    stackedS(:, i) = combinedFrame;
+end
+%%
+% Initialize W
+W = complex(zeros(K, L, nrmics));
+
+% Calculate Wiener filter weights
+for k = 1:K
+    for l = 1:L
+        % Wiener filter
+        W(k, l, :) = Sxx(k, l) ./ (Sxx(k, l) + Nxx(k, l));
+    end
 end
 
+%%
+
+
+% Initialize W with complex values
+%W = complex(rand(frameSize, numFrames, nrmics), rand(frameSize, numFrames, nrmics));
+
+% Perform the multiplication and sum
+for k = 1:K
+    for l = 1:L
+        % Element-wise multiplication and sum across the M dimension
+        stackedS(k, l) = sum(conj(W(k, l, :)) .* fftResult(k, l, :));
+    end
+end
+
+
+%%
 reconstructedSignal = zeros((numFrames-1) * stepSize + frameSize, 1);
 
 % Overlap-add method
@@ -69,5 +105,13 @@ for i = 1:numFrames
     % Overlap-add
     reconstructedSignal(frameStart:frameEnd) = reconstructedSignal(frameStart:frameEnd) + ifftFrame;
 end
-
-plot(reconstructedSignal)
+%%
+figure;
+plot(Clean, 'b', 'DisplayName', 'Original');
+hold on;
+plot(reconstructedSignal, 'r', 'DisplayName', 'Modeled');
+xlabel('Time');
+ylabel('Value');
+title('Original vs Modeled Time Series');
+legend('show');
+grid on;
